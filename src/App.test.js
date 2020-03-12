de23fd68e14e8417d1fd2ab437369887035bbe3a
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-03-12 04:15:10 +0100

    testing and refactoring

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,50 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import renderer from 'react-test-renderer';
+
+import App, { Search, Button } from './App';
+
+
+describe('App', () => {
+
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  
+  })
+  
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <App />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  
+  })
+  
+  })
+  
+
+describe('Search', () => {
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Search>Search</Search>, div);
   ReactDOM.unmountComponentAtNode(div);
+
+})
+
+test('has a valid snapshot', () => {
+  const component = renderer.create(
+    <Search>Search</Search>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+})
+
 })
